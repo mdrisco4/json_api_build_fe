0864df8b6f411624c0c8f095ac811249aaa90d45
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-01-01 16:22:02 -0500

    frontend set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+axios.get('http://localhost:8080/')
+.then(res =>{
+  console.log(res)
+})
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
