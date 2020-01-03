af2f31b7737157cc09bdc8bc016d7128a9a7f7eb
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-01-03 16:37:50 -0500

    end of class commit

--- src/Components/MovieDetails.js
+++ src/Components/MovieDetails.js
@@ -7,24 +7,35 @@ class MovieDetails extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            movie: ''
+            title: '',
+            release_date:'',
+            tagline: '',
+            runtime: '',
+            overview: '',
+            revenue: ''
           }
     }
     componentDidMount(){
-        const url = 'http://localhost:8080/movies/id'
-        const id = this.props.match.params.id;
+        // const url = 'http://localhost:8080/movies/id'
+        // const id = this.props.match.params.id;
 
         // axios.get(`http://localhost:8080//movies/${movie._id}`)
-        axios.get('http://localhost:8080/movies/id')
+
+        axios.get('http://localhost:8080/movies/' + this.props.match.params.id)
         // axios.get(url + `${id}`)
         .then(res =>{
-            console.log(res.data)
+            console.log(res)
             console.log(this.props.match.params.id)
           this.setState({
-            movie:res.data
+            title: res.title,
+            release_date: res.release_date,
+            tagline: res.tagline,
+            runtime: res.runtime,
+            overview: res.overview,
+            revenue: res.revenue
         })
         // console.log(this.props.match)
-        console.log(url + `${id}`)
+        // console.log(url + `${id}`)
         })
       }
     render() {
